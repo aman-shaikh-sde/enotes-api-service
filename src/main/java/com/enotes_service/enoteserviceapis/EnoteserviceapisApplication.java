8fc5ed731ddb001f480b4964d231077e3fe5a407
aman.shaikh.sde <amanshaikh05050@gmail.com>
2025-06-08 10:00:07 +0530

    Initial Commit

---
+++ src/main/java/com/enotes_service/enoteserviceapis/EnoteserviceapisApplication.java
@@ -0,0 +1,13 @@
+package com.enotes_service.enoteserviceapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnoteserviceapisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnoteserviceapisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enotes_service/enoteserviceapis/EnoteserviceapisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enotes_service.enoteserviceapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnoteserviceapisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
