338658c2120ee2e4ea4c35d8063605adb37c1e27
aman.shaikh.sde <amanshaikh05050@gmail.com>
2025-06-11 17:55:27 +0530

    Handle Exception

--- src/main/java/com/enotes_service/enoteserviceapis/Exception/ResourceNotFoundException.java
+++ src/main/java/com/enotes_service/enoteserviceapis/Exception/ResourceNotFoundException.java
@@ -1,7 +1,7 @@
 package com.enotes_service.enoteserviceapis.Exception;
 
 public class ResourceNotFoundException extends Exception{
-    public ResourceNotFound(String message){
+    public ResourceNotFoundException(String message){
         super(message);
     }
 }
